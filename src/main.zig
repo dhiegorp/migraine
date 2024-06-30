17f4fcb4ecf71876e771ca09e5982b0179996ef5
Dhiego Ramos <dhiegorp@gmail.com>
2024-06-30 20:28:17 -0300

    error message when source file was not found

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,11 @@
 const std = @import("std");
 const migraine = @import("migraine.zig");
+const err = std.io.getStdErr().writer();
+const out = std.io.getStdOut().writer();
+const in = std.io.getStdIn().reader();
 
 pub fn main() !void {
+    try out.print("Migraine 0.0.1a\n", .{});
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const allocator = arena.allocator();
     defer arena.deinit();
@@ -9,18 +13,15 @@ pub fn main() !void {
     //when using windows
 
     var args = try std.process.argsWithAllocator(allocator);
-
     _ = args.skip();
 
-    var path_arg: []const u8 = undefined;
-
-    if (args.next()) |arg| {
-        path_arg = arg;
+    if (args.next()) |path_arg| {
+        const loaded = try migraine.loadProgram(allocator, path_arg);
+        var diagnostics = migraine.InterpreterDiagnostics{ .detailed_message = undefined, .failed_opcode = undefined };
+        const interpreter = try migraine.Interpreter.init(allocator);
+        try interpreter.eval(loaded, in, out, &diagnostics);
+    } else {
+        try err.print("No source file found!\nUsage: \n\tmigraine <program.bf>\n", .{});
+        return;
     }
-
-    const interpreter = try migraine.Interpreter.init(allocator);
-    const loaded = try migraine.loadProgram(allocator, path_arg);
-    var diagnostics = migraine.InterpreterDiagnostics{ .detailed_message = undefined, .failed_opcode = undefined };
-
-    try interpreter.eval(loaded, std.io.getStdIn().reader(), std.io.getStdOut().writer(), &diagnostics);
 }
