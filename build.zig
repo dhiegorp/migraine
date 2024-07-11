bbd10e41931a10effe8579e94dd498069ffca772
Dhiego Ramos <dhiegorp@gmail.com>
2024-07-11 17:35:50 -0300

    including constants to be later used on release phase of executable

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const ARTIFACT_VERSION = @embedFile("./src/metadata/.version");
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
