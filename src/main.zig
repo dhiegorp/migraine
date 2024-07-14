259f0ef22e138f8352e556802a9957d8c348f671
Dhiego Ramos <dhiegorp@gmail.com>
2024-07-13 22:54:18 -0300

    implementing initial CLI

--- src/main.zig
+++ src/main.zig
@@ -4,14 +4,7 @@ const migraine = @import("migraine.zig");
 const argsproc = @import("argsproc.zig");
 const CLI = @import("cli.zig");
 
-///
-/// migraine --help
-///
 pub fn main() !void {
-    //const err = std.io.getStdErr().writer();
-    //const out = std.io.getStdOut().writer();
-    //const in = std.io.getStdIn().reader();
-
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const allocator = arena.allocator();
     defer arena.deinit();
